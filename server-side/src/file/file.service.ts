fec933f0e5c679db40680455b550c57e6acf8524
bee1oss <begahaciyew@gmail.com>
2025-05-07 08:57:39 +0100

    file system is end

--- server-side/src/file/file.service.ts
+++ server-side/src/file/file.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import path from 'path'
+import { FileResponse } from './file.interface'
+import { writeFile, ensureDir } from 'fs-extra'
 
 @Injectable()
-export class FileService {}
+export class FileService {
+	async saveFiles(files: Express.Multer.File[], folder: string = 'products') {
+		const uploadFolder = `${path}/uploads/${folder}`
+
+		await ensureDir(uploadFolder)
+
+		const resopse: FileResponse[] = await Promise.all(
+			files.map(async file => {
+				const originalName = `${Date.now()}-${file.originalname}`
+
+				await writeFile(`${uploadFolder}/${originalName}`, file.buffer)
+
+				return {
+					url: `/uploads/${folder}/${originalName}`,
+					name: originalName
+				}
+			})
+		)
+		return resopse
+	}
+}
